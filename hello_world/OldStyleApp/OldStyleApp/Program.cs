5f7ca708c888d79b460f909b5ad3938a815c94bd
Craig Rupp <craigtrupp8@gmail.com>
2024-02-16 12:03:44 -0700

    Now final push up here for hello world stuff

---
+++ hello_world/OldStyleApp/OldStyleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OldStyleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+            Console.Beep();
+            Console.WriteLine("After Ze Beep");
+            Console.Beep();
+        }
+    }
+}
